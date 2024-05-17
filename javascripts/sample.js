da62d7898e6bf75a8053d5701429c1c7ae184304
tatsukawa <tatsukawatakamasa@gmail.com>
2024-05-17 16:15:05 +0900

    Add all changes and new files

---
+++ javascripts/sample.js
@@ -0,0 +1,22 @@
+function emailValidation() {
+  const form = document.getElementById('form');
+  form.addEventListener('submit', function(e) {
+    // 条件分岐の前にsubmit処理をキャンセルしてしまう
+    e.preventDefault();
+    if(form.email.value !== form.email_confirm.value) {
+      const element = document.createElement('p')
+      const message = document.createTextNode("Eメールが一致しません")
+      form.appendChild(element);
+      element.appendChild(message);
+      element.classList.add("alert");
+      setTimeout(function() {
+        form.removeChild(element);
+      }, 3000);
+    } else {
+      // 一致した場合は再度submitを行う
+      form.submit();
+    }
+  });
+};
+
+window.onload = emailValidation;
